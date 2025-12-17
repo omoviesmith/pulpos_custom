21fe96878ad6ef1be5db78518bf7287253293d68
Smith Omovie <omoviesmith@gmail.com>
2025-12-16 18:20:23 -0800

    3 columns

--- pulpos_custom/hooks.py
+++ pulpos_custom/hooks.py
@@ -17,7 +17,7 @@ app_include_css = [
 app_include_js = "/assets/pulpos_custom/js/pulpos_custom.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/pulpos_custom/css/pulpos_custom.css"
+web_include_css = "/assets/pulpos_custom/css/website_products.css"
 # web_include_js = "/assets/pulpos_custom/js/pulpos_custom.js"
 
 # include custom scss in every website theme (without file extension ".scss")
